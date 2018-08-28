410ea53bd1a16d333006bce7c2900ee737edee8e
barekatf <42788411+barekatf@users.noreply.github.com>
2018-08-28 16:24:19 -0700

    Add files via upload

---
+++ BlueTooth Remote/Bluetooth_Test/Bluetooth_Test.ino
@@ -0,0 +1,47 @@
+/*
+COMMAND	        RESPONSE        COMMENT
+AT	        OK              Used to verify communication
+AT+VERSION	OKlinvorV1.8	The firmware version (version might depend on firmware)
+AT+NAMExyz	OKsetname	Sets the module name to “xyz”
+AT+PIN1234	OKsetPIN	Sets the module PIN to 1234
+AT+BAUD1	OK1200	        Sets the baud rate to 1200
+AT+BAUD2	OK2400	        Sets the baud rate to 2400
+AT+BAUD3	OK4800	        Sets the baud rate to 4800
+AT+BAUD4	OK9600	        Sets the baud rate to 9600
+AT+BAUD5	OK19200	        Sets the baud rate to 19200
+AT+BAUD6	OK38400	        Sets the baud rate to 38400
+AT+BAUD7	OK57600	        Sets the baud rate to 57600
+AT+BAUD8	OK115200	Sets the baud rate to 115200
+AT+BAUD9	OK230400	Sets the baud rate to 230400
+AT+BAUDA	OK460800	Sets the baud rate to 460800
+AT+BAUDB	OK921600	Sets the baud rate to 921600
+AT+BAUDC	OK1382400	Sets the baud rate to 1382400
+*/
+#include <SoftwareSerial.h>  
+
+int bluetoothTx = 2;  // TX-O pin of bluetooth mate, Arduino D2
+int bluetoothRx = 3;  // RX-I pin of bluetooth mate, Arduino D3
+
+SoftwareSerial bt (bluetoothTx, bluetoothRx); // RX, TX
+
+char cmd = 0; 
+char val = 0; 
+
+void setup() { 
+  Serial.begin(9600);  // Begin the serial monitor at 9600bps
+  bt.begin(115200);  // Start bluetooth serial at 115200
+}
+
+void loop() {
+  if(bt.available()) {
+    cmd = (char)bt.read();  
+    if (cmd == 'w') Serial.println("Forward"); 
+    if (cmd == 's') Serial.println("Backward"); 
+    if (cmd == 'd') Serial.println("Right"); 
+    if (cmd == 'a') Serial.println("Left");
+  }
+  if(Serial.available()) { 
+    val = (char)Serial.read(); 
+    bt. write(val); 
+  }
+}
---
+++ BlueTooth Remote/Processing_Send/Processing_Send.pde
@@ -0,0 +1,47 @@
+// The serial port:
+import processing.serial.*; 
+
+Serial myPort; 
+
+boolean forward = false; 
+boolean backward = false; 
+boolean right = false; 
+boolean left = false; 
+
+
+void setup() {
+  size(300,200);
+  colorMode(HSB, height, height, height);  
+  noStroke();
+  background(0);
+
+  // Open the port you are using at the rate you want:
+  myPort = new Serial(this, "COM6",115200);
+
+}
+
+void draw() { 
+  sendBT(); 
+}
+
+void sendBT() { 
+  if (forward) myPort.write('w');
+  if (backward) myPort.write('s');
+  if (right) myPort.write('d');
+  if (left) myPort.write('a');
+}
+
+void keyPressed() { 
+  print(key); 
+  if (key == 'w') forward = true; 
+  if (key == 's') backward = true; 
+  if (key == 'd') right = true;
+  if (key == 'a') left = true; 
+}
+
+void keyReleased() { 
+  forward = false; 
+  backward = false; 
+  right = false; 
+  left = false; 
+}
